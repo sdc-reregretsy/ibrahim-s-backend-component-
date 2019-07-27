6ec6a87d066e4d79b824406f07a9d71fb297abf2
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-07-27 10:44:05 -0500

    testing travis

---
+++ test/init-db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Running db seed commands"
+
+psql -d test_reregretsy_database -c "INSERT INTO product VALUES ('1213d10e-b6b5-4d6d-af44-2b10d334ed52','http://lorempixel.com/640/480/nature');" -U postgres

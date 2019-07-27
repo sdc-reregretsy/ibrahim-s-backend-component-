1adf1257f7ccb0c17da168d1537934cfcb03247e
ibrahim jabr <ibrahim.aljabr@gmail.com>
2019-07-27 11:29:24 -0500

    testing travis

--- DataBase/indexpg.js
+++ DataBase/indexpg.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg')
 const client = new Client({
-    database: 'retsy',
-    password: '121203',
+    database: 'retsy_database',
+    
   })
 client.connect()
 .then(() => console.log('Connected to db'))
